be0ada5128d40feab26eb84f8a66cfa2f9350395
Tam Nguyen <tam.nguyen.thanh@tpssoft.com>
2019-08-21 11:28:27 +0700

    Plugin generate

---
+++ Controller/Admin/ConfigController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Plugin\EC4MOMO\Controller\Admin;
+
+use Eccube\Controller\AbstractController;
+use Plugin\EC4MOMO\Form\Type\Admin\ConfigType;
+use Plugin\EC4MOMO\Repository\ConfigRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConfigController extends AbstractController
+{
+    /**
+     * @var ConfigRepository
+     */
+    protected $configRepository;
+
+    /**
+     * ConfigController constructor.
+     *
+     * @param ConfigRepository $configRepository
+     */
+    public function __construct(ConfigRepository $configRepository)
+    {
+        $this->configRepository = $configRepository;
+    }
+
+    /**
+     * @Route("/%eccube_admin_route%/ec4_momo/config", name="ec4_momo_admin_config")
+     * @Template("@EC4MOMO/admin/config.twig")
+     */
+    public function index(Request $request)
+    {
+        $Config = $this->configRepository->get();
+        $form = $this->createForm(ConfigType::class, $Config);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $Config = $form->getData();
+            $this->entityManager->persist($Config);
+            $this->entityManager->flush($Config);
+            $this->addSuccess('登録しました。', 'admin');
+
+            return $this->redirectToRoute('ec4_momo_admin_config');
+        }
+
+        return [
+            'form' => $form->createView(),
+        ];
+    }
+}
---
+++ TwigBlock.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Plugin\EC4MOMO;
+
+use Eccube\Common\EccubeTwigBlock;
+
+class TwigBlock implements EccubeTwigBlock
+{
+    /**
+     * @return array
+     */
+    public static function getTwigBlock()
+    {
+        return [];
+    }
+}
